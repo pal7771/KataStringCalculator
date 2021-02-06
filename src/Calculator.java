45b207f5da3f572dcb172fac3a1862e6ef5dad2f
pal7771 <surendrapaluikey@gmail.com>
2021-02-07 01:46:33 +0530

    added the code to pass the two comma separated value string

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,14 @@ public class Calculator {
     public int Add(String numbers) {
         if( numbers.length() == 0)
             return 0;
-        else
-            return Integer.parseInt( numbers );
+        else {
+            String[] str = numbers.split(",");
+
+            int sum = 0;
+            for(String s : str ){
+                sum += Integer.parseInt( s );
+            }
+            return sum;
+        }
     }
 }
