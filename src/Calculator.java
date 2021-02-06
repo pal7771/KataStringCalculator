d2bd14a12db305e111e5ff10657157d213a64cc2
pal7771 <surendrapaluikey@gmail.com>
2021-02-07 01:51:26 +0530

    added the code to pass the two value new line separated value string

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,12 +4,20 @@ public class Calculator {
         if( numbers.length() == 0)
             return 0;
         else {
-            String[] str = numbers.split(",");
-
+            String[] str1 = numbers.split(",");
+            String[] str2 = numbers.split("\n");
             int sum = 0;
-            for(String s : str ){
-                sum += Integer.parseInt( s );
+
+            if( str1.length > str2.length ){
+                for(String s : str1 ){
+                    sum += Integer.parseInt( s );
+                }
+            } else {
+                for(String s : str2 ){
+                    sum += Integer.parseInt( s );
+                }
             }
+
             return sum;
         }
     }
